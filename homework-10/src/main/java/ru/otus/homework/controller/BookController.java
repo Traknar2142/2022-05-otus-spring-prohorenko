3e0c7330e11e82b138a72b114ce9bdfe39864a32
vprokhorenko <viktor4516@yandex.ru>
2022-09-04 19:59:21 +0300

    Реализация дз №10

--- homework-10/src/main/java/ru/otus/homework/controller/BookController.java
+++ homework-10/src/main/java/ru/otus/homework/controller/BookController.java
@@ -3,61 +3,14 @@ package ru.otus.homework.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import ru.otus.homework.domain.Book;
-import ru.otus.homework.dto.BookDto;
-import ru.otus.homework.service.BookService;
-import ru.otus.homework.transformer.BookDtoTransformer;
-
-import java.util.List;
 
 @Controller
 public class BookController {
-    private final BookService bookService;
-
-    public BookController(BookService bookService) {
-        this.bookService = bookService;
-    }
 
     @GetMapping("/")
     public String listPage(Model model) {
-        model.addAttribute("keywords", "list users in Omsk, omsk, list users, list users free");
+        model.addAttribute("keywords", "some keywords");
         return "list";
     }
-
-    @GetMapping("/edit")
-    public String editPage(@RequestParam("id") Long id, Model model){
-        Book book = bookService.getById(id);
-        model.addAttribute("book", book);
-        return "edit";
-    }
-
-    /*@PostMapping("/edit")
-    public String updateBook(BookDto book){
-        Book entityBook = BookDtoTransformer.transformToEntity(book);
-        bookService.updateBook(entityBook);
-        return "redirect:/";
-    }
-
-    @GetMapping("/add")
-    public String addPage(Model model){
-        model.addAttribute("book", new Book());
-        return "add";
-    }
-
-    @PostMapping("/add")
-    public String saveBook(@ModelAttribute BookDto book){
-        Book entityBook = BookDtoTransformer.transformToEntity(book);
-        bookService.addBook(entityBook);
-        return "redirect:/";
-    }
-
-    @GetMapping("/delete")
-    public String deleteBook(@RequestParam("id") Long id){
-        bookService.deleteBook(id);
-        return "redirect:/";
-    }*/
 
 }
--- homework-10/src/main/java/ru/otus/homework/controller/RestBookController.java
+++ homework-10/src/main/java/ru/otus/homework/controller/RestBookController.java
@@ -1,22 +1,10 @@
 package ru.otus.homework.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import ru.otus.homework.domain.Book;
 import ru.otus.homework.dto.BookDto;
 import ru.otus.homework.service.BookService;
-import ru.otus.homework.transformer.BookDtoTransformer;
-import ru.otus.homework.transformer.BookTransformer;
 
 import java.util.List;
 
@@ -29,26 +17,8 @@ public class RestBookController {
     private final BookService bookService;
 
     @GetMapping("/api/books")
-    public List<BookDto> getAllBooks(){
+    public List<BookDto> getAllBooks() {
         return bookService.getAllBooks();
     }
 
-    @GetMapping("/api/books/{id}")
-    public ResponseEntity<BookDto> getBook(@PathVariable Long id){
-        Book entityBook = bookService.getById(id);
-        return ResponseEntity.ok(BookDtoTransformer.transformToDto(entityBook));
-    }
-
-    @PutMapping(value ="/api/books/update", consumes = MediaType.APPLICATION_JSON_VALUE, produces =
-            MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<BookDto> updateBook(@RequestBody BookDto bookDto){
-        Book entityBook = BookDtoTransformer.transformToEntity(bookDto);
-        Book book = bookService.updateBook(entityBook);
-        return ResponseEntity.ok(BookDtoTransformer.transformToDto(book));
-    }
-
-    @DeleteMapping("/api/books/delete/{id}")
-    public void deleteBook(@PathVariable Long id){
-        bookService.deleteBook(id);
-    }
 }
