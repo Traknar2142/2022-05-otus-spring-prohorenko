65e802d9c783d0d7b8a3aba7abe1306ec9d844aa
vprokhorenko <traknar2142@gmail.com>
2023-02-17 18:46:58 +0400

    Добавил алиас

--- homework-8/src/main/java/ru/otus/homework/repository/CommentRepository.java
+++ homework-8/src/main/java/ru/otus/homework/repository/CommentRepository.java
@@ -2,6 +2,7 @@ package ru.otus.homework.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.otus.homework.domain.Comment;
 
 import java.util.List;
@@ -10,6 +11,6 @@ import java.util.List;
  * @author Прохоренко Виктор
  */
 public interface CommentRepository extends MongoRepository<Comment, String> {
-    @Query(value = "{'book.id' : ?0}")
-    List<Comment> findCommentByBookId(String bookId);
+    @Query(value = "{'book.id' : :#{#bookId}}")
+    List<Comment> findCommentByBookId(@Param("bookId") String bookId);
 }
